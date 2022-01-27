894940d2748b395c18c40a37dd35aa32f0168dc8
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-01-27 21:47:02 +0100

    Added comments

--- script.js
+++ script.js
@@ -5,14 +5,20 @@ const walk = document.getElementById("walk");
 const countdownStart = document.getElementById("countdown-start");
 const countdownEnd = document.getElementById("countdown-end");
 
+//adding a listener to a button
 button.addEventListener("click", () => {
+  //randomizing the number of seconds a person needs to wait for a green light to appear
   let seconds = Math.floor(Math.random() * 5) + 2;
   countdownStart.innerHTML = "The time to green: " + seconds;
+  //interval which decreases the number of seconds till the green light
   let interval = setInterval(() => {
     seconds--;
     countdownStart.innerHTML = "The time to green: " + seconds;
   }, 1000);
 
+  //timeout which changes the lights after the random number of seconds expires, it also stops the interval
+  //and starts a new interval which keeps for 5 seconds + it starts a timeout which when is finished, it stops the interval
+  //and changes the ligths back
   setTimeout(() => {
     stop.className = "stop-off";
     walk.className = "walk-on";
