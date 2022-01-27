2baf61b53213849f928c558870b5b925c689a085
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-01-27 21:40:59 +0100

    Added descriptions showing how many seconds are left + the seconds were randomized

--- script.js
+++ script.js
@@ -2,12 +2,33 @@ const button = document.getElementById("button-cross");
 const stop = document.getElementById("stop");
 const walk = document.getElementById("walk");
 
+const countdownStart = document.getElementById("countdown-start");
+const countdownEnd = document.getElementById("countdown-end");
+
 button.addEventListener("click", () => {
-  stop.className = "stop-off";
-  walk.className = "walk-on";
+  let seconds = Math.floor(Math.random() * 5) + 2;
+  countdownStart.innerHTML = "The time to green: " + seconds;
+  let interval = setInterval(() => {
+    seconds--;
+    countdownStart.innerHTML = "The time to green: " + seconds;
+  }, 1000);
 
   setTimeout(() => {
-    stop.className = "stop-on";
-    walk.className = "walk-off";
-  }, 5000);
+    stop.className = "stop-off";
+    walk.className = "walk-on";
+    clearInterval(interval);
+    countdownStart.innerHTML = "";
+    let seconds2 = 5;
+    countdownEnd.innerHTML = "The time when there is green: " + seconds2;
+    let newInterval = setInterval(() => {
+      seconds2--;
+      countdownEnd.innerHTML = "The time when there is green: " + seconds2;
+    }, 1000);
+    setTimeout(() => {
+      stop.className = "stop-on";
+      walk.className = "walk-off";
+      clearInterval(newInterval);
+      countdownEnd.innerHTML = "";
+    }, 5000);
+  }, seconds * 1000);
 });
